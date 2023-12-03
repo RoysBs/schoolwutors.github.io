dba3018743d0f74d833452aa282f90be84765def
RoysBs <lepoardking123@gmail.com>
2023-12-03 18:46:50 -0500

    e

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-<!-- index.html -->
+// script.js
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Homework Helper</title>
-</head>
-<body>
-    <h1>Welcome to the Homework Helper Service!</h1>
-    <p>Subjects: <span id="subjects"></span></p>
-    <p>Availability: <span id="availability"></span></p>
-    <p>Rate: <span id="rate"></span></p>
-    <script src="script.js"></script>
-</body>
-</html>
+document.getElementById('subjects').innerText = "Math, Science, ELA, Social Studies";
+document.getElementById('availability').innerText = "After school hours";
+document.getElementById('rate').innerText = "$3 per session (one hour max)";
